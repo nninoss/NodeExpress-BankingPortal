1e9d50263a38fd948e65a7625bd4fcc24354bdde
nninoss <nninoss@gmail.com>
2020-02-05 12:05:19 -0600

    Completed Module2

--- src/app.js
+++ src/app.js
@@ -10,6 +10,37 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => res.render("index", { title: "Index" }));
+const accountData = fs.readFileSync(
+  path.join(__dirname, "json", "accounts.json"),
+  "utf8"
+);
+
+const userData = fs.readFileSync(
+  path.join(__dirname, "json", "users.json"),
+  "utf8"
+);
+
+const users = JSON.parse(userData);
+
+const accounts = JSON.parse(accountData);
+
+app.get("/", (req, res) =>
+  res.render("index", { title: "Account Summary", accounts })
+);
+
+app.get("/savings", (req, res) => {
+  res.render("account", { account: accounts.savings });
+});
+
+app.get("/checking", (req, rest) => {
+  res.render("account", { account: accounts.checking });
+});
+app.get("/credit", (req, ret) => {
+  res.render("account", { account: accounts.credit });
+});
+
+app.get("/profile", (req, rest) => {
+  res.render("profile", { user: users[0] });
+});
 
 app.listen(3000, () => console.log("PS Project Running on port 3000!"));
