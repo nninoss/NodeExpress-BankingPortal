4c68f1b4caf58ee68ba2a6f5ff29f0433cfe8194
nninoss <nninoss@gmail.com>
2020-02-05 22:53:19 -0600

    Completed Module 3

--- src/app.js
+++ src/app.js
@@ -47,10 +47,11 @@ app.post("/transfer", (req, res) => {
     accounts[req.body.from].balance - req.body.amount;
   accounts[req.body.to].balance =
     parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
+
   const accountsJSON = JSON.stringify(accounts, null, 4);
   fs.writeFileSync(
     path.join(__dirname, "json/accounts.json"),
-    accountJSON,
+    accountsJSON,
     "utf8"
   );
   res.render("transfer", { message: "Transfer Completed" });
