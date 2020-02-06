63619fe96c51b42f32eb4077b3630a23a6a4261b
nninoss <nninoss@gmail.com>
2020-02-06 15:04:11 -0600

    completed module5

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-consts { accounts } = require('../data'); 
+const { accounts } = require("../data");
 
 router.get("/savings", (req, res) => {
-    res.render("account", { account: accounts.savings });
-  });
-  
-router.get("/checking", (req, res) => {
-    res.render("account", { account: accounts.checking });
-  });
-  
-router.get("/credit", (req, res) => {
-    res.render("account", { account: accounts.credit });
-  });
+  res.render("account", { account: accounts.savings });
+});
 
-  module.exports = router;
+router.get("/checking", (req, res) => {
+  res.render("account", { account: accounts.checking });
+});
+
+router.get("/credit", (req, res) => {
+  res.render("account", { account: accounts.credit });
+});
+
+module.exports = router;
